e95ce2dc1872ddcecd160e04d4ca3c63adf741be
Jose Fernandez (magec) <joseferper@gmail.com>
2016-01-29 12:07:01 +0100

    Initial implementation

--- capistrano-consul.gemspec
+++ capistrano-consul.gemspec
@@ -27,8 +27,8 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ['lib']
 
-  gem.add_dependency 'capistrano', '>= 3.1'
-  gem.add_dependency 'diplomat', '~> 0.15.0'
+  spec.add_dependency 'capistrano', '>= 3.1'
+  spec.add_dependency 'diplomat', '~> 0.15.0'
   spec.add_development_dependency 'bundler', '~> 1.11'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
--- lib/capistrano/consul.rb
+++ lib/capistrano/consul.rb
@@ -1,7 +1,25 @@
-require "capistrano/consul/version"
+require 'capistrano/consul/version'
+require 'diplomat'
 
 module Capistrano
   module Consul
-    # Your code goes here...
+    def self.setup
+      return if @url
+      @url = fetch(:consul_url)
+      return false unless @url
+
+      Diplomat.configure do |config|
+        config.url = @url
+      end
+    end
+  end
+
+  module DSL
+    def consul_service(service_name, properties = {})
+      Consul.setup
+      Diplomat::Service.get(service_name, :all).each do |node|
+        server(node['Address'], properties)
+      end
+    end
   end
 end
