bd55b134d2504ae02cb33d2006622dd6c745f87b
Jose Fernandez (magec) <joseferper@gmail.com>
2016-04-20 12:34:45 +0200

    Added a new operation all_nodes

--- lib/capistrano/consul.rb
+++ lib/capistrano/consul.rb
@@ -15,6 +15,13 @@ module Capistrano
   end
 
   module DSL
+    def consul_all_nodes(properties = {})
+      Consul.setup
+      Diplomat::Node.get_all.each do |node|
+        server(node['Address'], properties)
+      end
+    end
+
     def consul_service(service_name, properties = {})
       Consul.setup
       Diplomat::Service.get(service_name, :all).each do |node|
